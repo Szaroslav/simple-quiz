d9427e80d7313fd93f30a3b30fa97537487cea22
Jakub <jakub.szaredko@gmail.com>
2021-06-07 21:50:33 +0200

    Create result score animation, change index.html

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const correctAnswers = ['a', 'a', 'b', 'b', 'b'];
 const form = document.querySelector('.quiz__form');
+const result = document.querySelector('.quiz-result');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const answers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    const answers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
     let score = 0;
 
     answers.forEach((answer, i) => {
-        if (answer == correctAnswers[i]) score++;
+        if (answer === correctAnswers[i]) score += (1 / correctAnswers.length) * 100;
     });
 
-    console.log(score);
+    scrollTo(0, 0);
+    result.classList.remove('d-none');
+
+    const resultPercentage = result.querySelector('span');
+    let intervalScore = 0;
+
+    setTimeout(() => {
+        const intervalId = setInterval(() => {
+            resultPercentage.textContent = `${intervalScore} %`;
+            if (intervalScore++ === score) clearInterval(intervalId);
+        }, 10);
+    }, 500);
 });
